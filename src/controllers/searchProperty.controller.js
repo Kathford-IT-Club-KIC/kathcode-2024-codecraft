13d53b089b3ae6d34864c9da63091cff4483bdc7
Anuroop Tater <anuroop.231209@ncit.edu.np>
2024-06-28 22:27:12 +0545

    Search regex

--- src/controllers/searchProperty.controller.js
+++ src/controllers/searchProperty.controller.js
@@ -16,5 +16,21 @@ const searchProperty = asyncHandler(async (req, res, next) => {
     const searchRegex = new RegExp(query, "i");
 
     try{
+        // Search properties by title, description, address, or categories
+        const properties = await Product.find({
+            $or: [
+                { title: searchRegex },
+                { description: searchRegex },
+                { address: searchRegex },
+                { categories: searchRegex },
+            ],
+        });
+
+        // If no properties are found, response with a 404 status
+        if (!properties.length == 0) {
+            return res
+            .status(404)
+            .json(new ApiResponse(404, "No properties found matching your query"));
+        }
+
         
-    }
