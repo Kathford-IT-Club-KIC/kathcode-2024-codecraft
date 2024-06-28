6d44b005dad97e16e3b9b6636c436568240b956e
Babita00 <babitaasth07@gmail.com>
2024-06-29 00:54:23 +0545

    property fetching completed

--- src/controllers/fetchProperty.controller.js
+++ src/controllers/fetchProperty.controller.js
@@ -1,32 +1,30 @@
 import { Property } from "../models/Property.models.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/apiError.js";
+import { apiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 
 // Function to fetch all properties
 const getAllProperties = asyncHandler(async (req, res) => {
-    const properties = await Property.find();
-    res.status(200).json({
-        success: true,
-        data: properties,
-    });
+  const properties = await Property.find();
+  res.status(200).json({
+    success: true,
+    data: properties,
+  });
 });
 
 // Function to fetch a single property by ID
 const getPropertyById = asyncHandler(async (req, res, next) => {
-    const property = await Property.findById(req.params.id);
+  const property = await Property.findById(req.params.id);
 
-    if (!property) {
-        return next(new ApiError(404, "Property not found"));
-    }
+  if (!property) {
+    return next(new apiError(404, "Property not found"));
+  }
 
-    return res.status(200).json({
-        success: true,
-        message: "Property fetched successfully",
-        data: property,
-    });
+  return res.status(200).json({
+    success: true,
+    message: "Property fetched successfully",
+    data: property,
+  });
 });
 
 export { getAllProperties, getPropertyById };
-
-
