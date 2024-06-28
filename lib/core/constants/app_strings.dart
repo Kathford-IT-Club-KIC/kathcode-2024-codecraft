28078dd095b209f9659a2e55ffcc508817ba69bf
Aaditya Binod Yadav <aadityarayyadav@gmail.com>
2024-06-29 01:00:05 +0545

    appstrings

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,33 @@
+class AppStrings {
+  AppStrings._();
+
+  static var appName = 'Easy Ghar';
+
+  static var noInternetConnection =
+      """ It seems there is something wrong with your internet connection.
+      Please connect to the internet and try again.""";
+  static String loadingTxt = 'Loading...';
+
+  //first splash screen
+static String findBetterToSettle = "Find Better To Settle";
+static String LetUsFindYour = "Let's Find your";
+static String dreamRealState = "Dream Real Estate";
+static String splashScreenSubTextOne =
+    "Where dreams meet reality, \nFind your perfect property";
+static String getStarted ="Get Started";
+
+//Dashboard
+  static String homeIcon = "Home";
+  static String searchIcon = "Search";
+  static String favouritesIcon = " Favourites";
+  static String connectIcon = "Connect";
+  static String accountIcon = "Account";
+
+  static String loremTxt=
+      'Lorem ipsum dolor sit amet consectetur. Amet id proin laoreet eu'
+      ' rhoncus consectetur orci scelerisque sit. Sit ridiculus est tellus'
+       ' sem erat elementum interdum ac. Sit ridiculus est tellus sem erat'
+        'elementum interdum ac.';
+
+
+}
