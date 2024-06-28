22c58df770b42ca201e24e7815825c3b9335d7b8
Babita00 <babitaasth07@gmail.com>
2024-06-28 20:38:39 +0545

    password change update

--- src/controllers/user.passwordChange.js
+++ src/controllers/user.passwordChange.js
@@ -5,14 +5,16 @@ import { ApiResponse } from "../utils/apiResponse.js";
 
 const changePassword = asyncHandler(async (req, res) => {
   const { oldpassword, newPassword } = req.body;
-  const user = await User.findById(req.user?._id);
 
-  if (!user) {
-    throw new apiError(409, "Invalid word password");
+  const user = await User.findById(req.user?._id);
+  const isPasswordCorrect = await user.isPasswordCorrect(oldpassword);
+
+  if (!isPasswordCorrect) {
+    throw new apiError(401, "You've entered  wrong password");
   }
+
   user.password = newPassword;
   await user.save({ validateBeforeSave: false });
-
   return res
     .status(200)
     .json(new ApiResponse(200, {}, "Password changed successfully"));
