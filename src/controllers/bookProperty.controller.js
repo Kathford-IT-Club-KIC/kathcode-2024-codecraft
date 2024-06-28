f49837334b7de176ef171314367f2c0d08151ec9
Anuroop Tater <anuroop.231209@ncit.edu.np>
2024-06-28 19:43:45 +0545

    Adding a new booking

---
+++ src/controllers/bookProperty.controller.js
@@ -0,0 +1,25 @@
+import { Booking } from "";
+import { Property } from "";
+import { User } from "";
+
+//Add a new booking
+const bookProperty = async (req, res) => {
+    try {
+        const { propertyId, userId, startDate, endDate } = req.body;
+
+        //Validate property and user existence
+        const property = await Property.findById(propertyId);
+        if (!property) {
+            return res.status(404).json({ message: "Property not found" });
+        }
+
+        if (property.status === "booked") {
+            return res.status(400).json({ message: "Property is already booked" });
+        }
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+}
+}
