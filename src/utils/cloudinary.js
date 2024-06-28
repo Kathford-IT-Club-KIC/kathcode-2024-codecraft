c4475c8ce979dc12ded82682b0bdeb0f8a26446a
Babita00 <babitaasth07@gmail.com>
2024-06-28 22:41:44 +0545

    cloudinary update

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,17 +1,18 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
-const uploadCloudinary = async (localFilepath) => {
+
+const uploadCloudinary = async (localFilepath, publicId) => {
   try {
     if (!localFilepath) return null;
-    //upload local file to cloudinary
-    const response = await cloudinary.uploadCloudinary.upload(localFilepath, {
+    // upload local file to cloudinary
+    const response = await cloudinary.uploader.upload(localFilepath, {
       resource_type: "auto",
       public_id: publicId,
     });
-    fs.unlinSync(localFilepath);
+    fs.unlinkSync(localFilepath);
     return response;
   } catch (error) {
-    console.log("Error occured while uploading on Cloudinary");
+    console.log("Error occurred while uploading on Cloudinary");
     fs.unlinkSync(localFilepath);
     throw error;
   }
