779a4d61d55021c8fc84a024c24b7cad1389113c
Anuroop Tater <anuroop.231209@ncit.edu.np>
2024-06-28 11:29:41 +0545

    Added

--- models/user.models.js
+++ models/user.models.js
@@ -17,7 +17,6 @@ const userSchema = new mongoose.Schema(
         },
         lastname: {
             type: String,
-            required: [true, "Password is required"],
         },
         password: {
             type: String,
@@ -32,4 +31,30 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true },
 );
+
+userSchema.pre("save", async function(next){
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+// checking if password is correct
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function (){
+    try{
+        //'this' refers to the current user interface
+        const token = jwt.sign(
+            {_id: this_id, email: this,email, username: this.username },
+            process.env.JWT_SECRET,
+            {
+                expiresIn :
+            }
+        )
+    }
+}
+
 
