357696dd20c34bfca87979537489b3b9a2b38c6e
srishtitater <srishtitater@gmail.com>
2024-06-28 22:16:46 +0545

    NEW propert1

--- lib/features/seller/model/property_info_model.dart
+++ lib/features/seller/model/property_info_model.dart
@@ -20,7 +20,15 @@ PropertyInfoModel(
   required this.address,
   required this.contactNumber,
   required this.amount,
+this.propertyType='House',
+this.listingType='For Sale',
+this.bedroomCount =1,
+this.bathroomCount=1,
+this.kitchenCount = 1,
+this.diningCount = 1,
+this.images = const [],
+this.utilities = const{},
 
 }
-)
+);
 
--- lib/features/seller/view/AddPropertyStep1.dart
+++ lib/features/seller/view/AddPropertyStep1.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hackathon_easyghar/features/seller/model/property_info_model.dart';
 
+import '../../../core/constants/app_colors.dart';
+
 class Addpropertystep1 extends StatefulWidget {
   const Addpropertystep1({super.key});
 
@@ -45,7 +47,144 @@ class _Addpropertystep1State extends State<Addpropertystep1> {
             address: _addressController.text,
             contactNumber: _priceController.text,
             amount: _priceController.text,);
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => AddPropertyStep2(propertyInfo: propertyInfo),
+          ),
+        );
       }
-    return const Placeholder();
+  }
+  @override
+  Widget build(BuildContext context)
+  return Scaffold(appBar: AppBar(
+  loading: IconButton(
+  icon: Icon(Icons.arrow_back),
+  onPressed: ()
+  {
+    Navigator.pop(context);
+  },
+  ),
+  title: Text(
+  'Add Property',
+  style: TextStyle(color: AppColors.white),
+  ),
+  ),
+  body: Padding(
+  padding: const EdgeInsets.all(16.0),
+  child: Column(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: [
+    if(showError)
+      Container(
+        color: Colors.red,
+        padding: EdgeInsets.all(6.0),
+        child: Row(
+  children: [
+  Icon(Icons.error, color: Colors.white),
+  SizedBox(width: 8),
+  Text(
+  'Error!!! Title is required',
+  style: TextStyle(color: Colors.white),
+  ),
+  ],
+
+  ),
+  ),
+  SizedBox(height: 8),
+  Text('Information',
+  style: TextStyle(fontWeight: FontWeight.bold,
+  color: Colors.black),),
+  SizedBox(height: 8),
+  Text(
+  'Give a brief but detailed information about the property',
+  style: TextStyle(color: Colors.black),
+  ),
+  SizedBox(height: 16),
+  TextField(
+  controller: _titleController,
+  style: TextStyle(color: Colors.grey),
+  decoration: InputDecoration(
+  labelText: 'TITLE',
+  border: OutlineInputBorder(
+  borderRadius: BorderRadius.circular(7.0),
+  ),
+  ),
+  ),
+  SizedBox(height: 16),
+  TextField(
+  controller: _descriptionController,
+  maxLines: 4,
+  style: TextStyle(color: Colors.black),
+  decoration: InputDecoration(
+  labelText: 'DESCRIPTION',
+  hintStyle: TextStyle(color: Colors.black),
+  hintText: 'Please enter a description to get the best possible response to your listing. Tell other users what you love about the property, what is great about it and its location',
+  border: OutlineInputBorder(
+  borderRadius: BorderRadius.circular(7.0),
+  ),
+  ),
+  ),
+  SizedBox(height: 16),
+  TextField(
+  controller: _addressController,
+  style: TextStyle(color: Colors.black),
+  decoration: InputDecoration(
+  labelText: 'ADDRESS',
+  border: OutlineInputBorder(
+  borderRadius: BorderRadius.circular(7.0),
+  ),
+  ),
+  ),
+  SizedBox(height: 16),
+  TextField(
+  controller: _contactNumberController,
+  style: TextStyle(color: Colors.black),
+  decoration: InputDecoration(
+  labelText: 'CONTACT NUMBER',
+  border: OutlineInputBorder(
+  borderRadius: BorderRadius.circular(7.0),
+  ),
+  ),
+  ),
+  SizedBox(height: 16),
+  TextField(
+  controller: _priceController,
+  style: TextStyle(color: Colors.black),
+  decoration: InputDecoration(
+  labelText: 'PRICE',
+  border: OutlineInputBorder(
+  borderRadius: BorderRadius.circular(7.0),
+  ),
+  ),
+  ),
+  ],
+  ),
+  ),
+  bottomNavigationBar: BottomAppBar(
+  child: Padding(
+  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+  child: Row(
+  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  children: [
+    TextButton(onPressed: (){
+      Navigator.pop(context);
+  }, child: Text('BACK',style: TextStyle(fontWeight: FontWeight.bold)),
+  ),
+  Text('STEP 1 OF 4'),
+  TextButton(
+  onPressed: () {
+  _navigateToNextPage(context);
+  },
+  child: Text('NEXT', style: TextStyle(fontWeight: FontWeight.bold)),
+  ),
+  ],
+  ),
+  ),
+  ),
+  );
+
+}
+
   }
 }
