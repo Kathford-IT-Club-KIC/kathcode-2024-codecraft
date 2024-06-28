27d0ca474d221b9a20a1a13d0b1cbe941a018a75
Anuroop Tater <anuroop.231209@ncit.edu.np>
2024-06-28 18:22:28 +0545

    Property Image upload

---
+++ src/controllers/addProperty.controller.js
@@ -0,0 +1,35 @@
+import { Property } from "../models/Property.models.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { parseAndValidateLocation } from "../parseAndValidateLocation.controller.js";
+
+const uploadPropertyImages= asyncHandler(async (req, res, next) => {
+    const propertyImagefiles = req.files?.propertyImage;
+    const propertyImageUrls = await handleImageUpload(propertyImagefiles, next);
+    if (!propertyImageUrls) return;
+    return { imageUrls: propertyImageUrls };
+});
+
+//New function to handle image upload
+const handleImageUpload = async (files, next) => {
+    if (files || files.length == 0){
+        return next(new ApiError(400, "At least one property image is required"));
+    }
+    try{
+        return await Promise.all(
+            files.map(async (file) => {
+                try{
+                   const imageUrl = await uploadOnCloudinary(file.path);
+                   return imageUrl.url;
+                } catch (error){
+                    console.error("Cloudinary Upload Error", error);
+                    throw new ApiError(500, "Error uploading image is Cloudinary");
+                }
+            }),
+        );
+} catch (error) {
+    console.error("Image Upload Error", error);
+    throw new ApiError(500, `Error uploading image: ${error.message}`);
+    }
+};
