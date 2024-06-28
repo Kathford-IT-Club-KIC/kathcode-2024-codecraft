5c5d59da766ce4ad35c315fad1c03c1e6a18632c
Aaditya Binod Yadav <aadityarayyadav@gmail.com>
2024-06-28 17:17:16 +0545

    recentlyadded2

--- lib/features/home/recently_added.dart
+++ lib/features/home/recently_added.dart
@@ -13,6 +13,22 @@ classa RecentlyAdded extends StatelessWidget{
     return Column(
     mainAxisAlignment: MainAxisAlignment.start,
     crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      AppSpacing.verticalSpaceLarge,
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          child: CText(
+          "Recently Added",
+          type: TextType.displayLarge,
+          color: AppColors.darkBlue,
+          ), //CText
+          ), //Padding
+          ///Horizontal property list
+          Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          )
+)
+]
     )
 }
 }
