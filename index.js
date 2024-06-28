80b0d185cc1ff9045e1003e65ccb5be7dc23738d
Anuroop Tater <anuroop.231209@ncit.edu.np>
2024-06-28 13:14:15 +0545

    Database connection setup added

---
+++ index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import connectDB from "./db/dataBaseConnect.js";
+import server from "./app.js";
+
+dotenv.config({
+    path:"./.env",
+});
+
+//Connect to the database
+connectDB()
+    .then(() => {
+        server.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MongoDB connection FAILED", err);
+    });
