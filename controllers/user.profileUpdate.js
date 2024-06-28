67425ff607ae50b17c67947edf07d4e086763a58
Babita00 <babitaasth07@gmail.com>
2024-06-28 14:04:03 +0545

    code for updating the iser profile

---
+++ controllers/user.profileUpdate.js
@@ -0,0 +1,47 @@
+import { asyncHandler } from "../utils/asyncHandler.ja";
+import { User } from "../models/user.models.js";
+const updateProfile = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { firstname, lastname, email, password } = req.body;
+    // Validation
+    if (
+      [firstname, lastname, email, password].some(
+        (field) => field?.trim() === ""
+      )
+    ) {
+      throw new ApiError(400, "All fields are required");
+    }
+
+    //validate if user exist or not
+    const user = await User.findById(id);
+    if (!user) {
+      throw new ApiError(409, "User not found");
+    }
+
+    //check if email is available
+    const takenEmail = await User.findOne({ email, _id: { $ne: id } });
+    if (takenEmail) {
+      throw new ApiError(409, "Email is already taken");
+    }
+
+    //update the information
+    user.firstname = firstname;
+    user.lastname = lastname;
+    user.email = email;
+    if (password) {
+      user.password = password; // Assuming password hashing middleware is in place
+    }
+    await user.save();
+    //return the response
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "User updated successfully", user));
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error occur while updating profile", error });
+  }
+});
+export { updateProfile };
