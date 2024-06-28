10d40d19faffe857accff3363c1ab07bd2c11d70
Babita00 <babitaasth07@gmail.com>
2024-06-29 00:31:13 +0545

    debugging in booking model

--- src/models/Booking.models.js
+++ src/models/Booking.models.js
@@ -1,32 +1,32 @@
 import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema(
-    {
-        property: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Property",
-            required: true,
-        },
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-            },
-        startDate: {
-                type: Date,
-                required: false,
-        },
-        endDate: {
-            type: Date,
-            required: false,
-        },
-        status:{
-            type: String,
-            enum: ["Booked", "Cancelled"],
-            default: "Booked",
-        },
+  {
+    property: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Property",
+      required: true,
     },
-    { timestamps: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: false,
+    },
+    endDate: {
+      type: Date,
+      required: false,
+    },
+    status: {
+      type: String,
+      enum: ["Booked", "Cancelled"],
+      default: "Booked",
+    },
+  },
+  { timestamps: true }
 );
 
 export const Booking = mongoose.model("Booking", bookingSchema);
