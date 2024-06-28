1f5f9ee07fb098464ab65ca4a0071a56edc7136b
Aaditya Binod Yadav <aadityarayyadav@gmail.com>
2024-06-28 17:12:06 +0545

    recentlyadded1

---
+++ lib/features/home/recently_added.dart
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+classa RecentlyAdded extends StatelessWidget{
+  const RecentlyAdded({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    )
+}
+}
