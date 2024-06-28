63c9626a816e8cd15c1bdf0c618d6e02ae2f8a38
Babita00 <babitaasth07@gmail.com>
2024-06-28 14:03:43 +0545

    login code updated

--- controllers/user.login.js
+++ controllers/user.login.js
@@ -5,24 +5,28 @@ import { apiResponse } from "../utils/apiResponse.js";
 import bcrypt from "bcryptjs";
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-  //validation
-  if ([email, password].some((field) => field?.trim() === "")) {
-    throw new ApiError(400, "All fields are required");
-  }
+  try {
+    //validation
+    if ([email, password].some((field) => field?.trim() === "")) {
+      throw new ApiError(400, "All fields are required");
+    }
 
-  // Check if user exists
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw new ApiError(401, "Invalid email or password");
-  }
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new ApiError(401, "Invalid email or password");
+    }
 
-  //check password
-  const isMatch = await bcrypt.compare(password, User.password);
-  if (!isMatch) {
-    throw new ApiError(401, "Invalid email or password");
+    //check password
+    const isMatch = await bcrypt.compare(password, User.password);
+    if (!isMatch) {
+      throw new ApiError(401, "Invalid email or password");
+    }
+    //response the success message
+    return res.status(201).json(new apiResponse(200, "Login successfully"));
+  } catch (error) {
+    return res.status(500).json({ message: "Error occur while login", error });
   }
-  //response the success message
-  return res.status(201).json(new apiResponse(200, "Login successfully"));
 });
 
 export { loginUser };
