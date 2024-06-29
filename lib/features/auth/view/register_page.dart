e33fc42dc88feec6b7ac8e12ba014646dc42e914
srishtitater <srishtitater@gmail.com>
2024-06-29 07:06:32 +0545

    Final

--- lib/features/auth/view/register_page.dart
+++ lib/features/auth/view/register_page.dart
@@ -5,6 +5,7 @@ import 'package:hackathon_easyghar/core/constants/constants.dart';
 import 'package:hackathon_easyghar/core/widgets/forms/cust_text_field.dart';
 import 'package:hackathon_easyghar/core/widgets/forms/field_validators.dart';
 import 'package:hackathon_easyghar/core/widgets/text/custom_text.dart';
+import 'package:hackathon_easyghar/features/auth/view/login_page.dart';
 
 import '../../../core/constants/app_assets.dart';
 
@@ -87,12 +88,128 @@ class _RegisterPageState extends State<RegisterPage>{
                     hintText: 'Email',
                     prefixIcon: Icon(Icons.email,color:AppColors.darkBlue),
                     controller: usernameController,
-                    validator: (value)=> FieldValidators.validateEmail(value),),
+                    validator: (value)=> FieldValidators.validateEmail(value),
+                    ),
+                    AppSpacing.verticalSpaceLarge,
+                    CustTextField(
+                        labelText:'Password',
+                        hintText: 'Password',
+                        controller: passwordController,
+                      prefixIcon: Icon(Icons.lock, color: AppColors.darkBlue),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _obscureText ? Icons.visibility : Icons.visibility_off,
+                          color: AppColors.darkBlue,
+                        ),
+                        onPressed: _togglePasswordVisibility,
+                      ),
+                      obscureText: _obscureText,
+                      validator: (value)=>
+                      FieldValidators.validatePassword(value),
+                    ),
+                    AppSpacing.verticalSpaceLarge,
+                    CustTextField(
+                      labelText:'Confirm Password',
+                      hintText: 'Confirm Password',
+                      controller: passwordController,
+                      prefixIcon: Icon(Icons.lock, color: AppColors.darkBlue),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _obscureText ? Icons.visibility : Icons.visibility_off,
+                          color: AppColors.darkBlue,
+                        ),
+                        onPressed: _togglePasswordVisibility,
+                      ),
+                      obscureText: _obscureText,
+                      validator: (value){
+                        if (value!=passwordController.text)
+                          {
+                            return 'Passwords do not match';
+                          }
+                        return null;
+                      },
+                    ),
+                    AppSpacing.verticalSpaceLarge,
+                    ElevatedButton(onPressed: (){
+                      if(_formKey.currentState!.validate()){
+                        AppMethods.showLoaderDialog(context);
+                        _registerUser();
+                      }
+                    },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.darkBlue,
+                        padding: EdgeInsets.symmetric(
+                            vertical: 14.0, horizontal: 80.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      child: Text(
+                        'Register',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.white,
+                        ),
+                      ),
+                        ),
+                    AppSpacing.vericalSpaceLarge,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CText('Already have account?',
+                        type: TextType.bodyMedium,
+                        color: AppColors.secondaryText,),
+                        TextButton(onPressed: (){Navigator.pop(context);},
+                            child: CText('Login',
+                            type: TextType.bodyMedium,
+                            color: AppColors.teal,
+                            fontWeight: FontWeight.bold,
+                            ),
+                        ),
+                      ],
+
+                    ),
                   ],
-                ))
+                ),
+            ),
+            if(_registerFuture !=null)
+  FutureBuilder<Response>(
+  future: _registerFuture,
+  builder: (context, snapshot) {
+  if (snapshot.connectionState == ConnectionState.waiting) {
+  return Container();
+  } else if (snapshot.hasError) {
+  Navigator.pop(context); // Hide the loader
+  AppMethods.displayToastFailure(
+  message: 'Registration failed: ${snapshot.error}');
+  return Text('Error: ${snapshot.error}');
+  } else if (snapshot.hasData) {
+  final response = snapshot.data!;
+
+  Navigator.pop(context);
+  if (response.statusCode == 200
+  || response.statusCode == 201)
+  {
+  AppMethods.displayToastSuccess(
+  message: 'Registration successful');
+  WidgetsBinding.instance.addPostFrameCallback((_){
+  Navigator.push(context,
+  MaterialPageRoute(builder: (builder) => LoginScreen()));
+  });
+  else{
+  AppMethods.displayToastFailure(
+  message: 'Registration failed, please try again');
+  }
+  }
+  return Container();
+  },
+      ),
+    }
+
           ],
         ),
       ),
-    )
+    );
 }
 }
